c8dc6090548e2d65afe9e427135af0168840f4b5
nester-dev <nester.dev@gmail.com>
2023-12-21 10:04:39 +0300

    fix: change login/register DTO

--- src/users/dto/user-login.dto.ts
+++ src/users/dto/user-login.dto.ts
@@ -1,10 +1,9 @@
-import { IsString, Length } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class UserLoginDto {
 	@IsString({ message: 'Invalid login' })
 	identifier: string;
 
 	@IsString({ message: 'Invalid password' })
-	@Length(8, 20, { message: 'Password must be between 8 and 20 characters' })
 	password: string;
 }
--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, Length } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class UserRegisterDto {
 	@IsEmail({}, { message: 'Invalid email' })
@@ -8,7 +8,7 @@ export class UserRegisterDto {
 	username: string;
 
 	@IsString({ message: 'Invalid password' })
-	@Length(8, 20, { message: 'Password must be between 8 and 20 characters' })
+	@IsNotEmpty({ message: 'Password is required' })
 	password: string;
 
 	@IsString({ message: 'Invalid firstName' })
